4746cc9fd9bca203d41e74360238c98f8d52ece1
pfeiring <pfeiring@live.com>
2015-02-18 12:17:28 +0100

    Additional log levels and renaming

--- src/log/log.go
+++ src/log/log.go
@@ -7,11 +7,13 @@ import (
 //-----------------------------------------------//
 
 const (
-	LOG_LEVEL_ERROR 	int 	= 	1
-	LOG_LEVEL_DEBUG 	int 	= 	2
+	LOG_NONE 		int 	= 		iota
+	LOG_ERROR 		int 	= 		iota
+	LOG_WARNING		int 	= 		iota
+	LOG_ALL			int 	= 		iota
 );
 
-var logLevel = LOG_LEVEL_DEBUG;
+var logLevel = LOG_ALL;
 
 func SetLogLevel(newLogLevel int) {
 	logLevel = newLogLevel;
@@ -20,13 +22,19 @@ func SetLogLevel(newLogLevel int) {
 //-----------------------------------------------//
 
 func Error(value interface{}) {
-	if logLevel >= LOG_LEVEL_ERROR {
+	if logLevel >= LOG_ERROR {
 		fmt.Println(value);
 	}
 }
 
-func Debug(value interface{}) {
-	if logLevel >= LOG_LEVEL_DEBUG {
+func Warning(value interface{}) {
+	if logLevel >= LOG_WARNING {
+		fmt.Println(value);
+	}
+}
+
+func Data(value interface{}) {
+	if logLevel >= LOG_ALL {
 		fmt.Println(value);
 	}
 }
