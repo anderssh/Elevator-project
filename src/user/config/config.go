64d93614cad9d8fbf2697ff8357b5268b6389ba0
pfeinrg <pfeinrg@live.com>
2015-04-19 11:01:00 +0200

    Some clean in config. Also added number of floors.

--- src/user/config/config.go
+++ src/user/config/config.go
@@ -5,15 +5,11 @@ import(
 );
 
 const(
-	MASTER_ALIVE_NOTIFICATION_DELAY 	time.Duration 	=	time.Millisecond * 100
-	MASTER_ALIVE_NOTIFICATION_TIMEOUT 	time.Duration 	=	time.Millisecond * 400
+	MASTER_ALIVE_NOTIFICATION_DELAY 	time.Duration 	=	time.Millisecond * 40
 
-	SLAVE_ALIVE_NOTIFICATION_DELAY 		time.Duration 	=	time.Millisecond * 40
-	SLAVE_ALIVE_NOTIFICATION_TIMEOUT 	time.Duration 	=	time.Millisecond * 200
+	NUMBER_OF_FLOORS		 			int 			= 	4
 
-	TIMEOUT_TIME_COST_RESPONSE 			time.Duration 	=	time.Millisecond * 200	
-
-	SHOULD_DISPLAY_ELEVATOR 			bool 			= 	true
+	SHOULD_DISPLAY_ELEVATOR 			bool 			= 	false
 	SHOULD_DISPLAY_WORKERS				bool 			= 	false
 
 	PORT_SERVER_DEFAULT 				int 			= 	9125
