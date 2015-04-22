a3c08dbec48bb8967251e4dead775f84fec3d173
pfeinrg <pfeinrg@live.com>
2015-04-22 08:26:28 +0200

    Clean up in config file

--- src/user/config/config.go
+++ src/user/config/config.go
@@ -5,26 +5,36 @@ import(
 );
 
 const(
-	DISTRIBUTOR_ALIVE_NOTIFICATION_DELAY 				time.Duration 	=	time.Millisecond * 40
-	DISTRIBUTOR_CONNECTION_CHECK_DELAY 					time.Duration 	=	time.Millisecond * 80
-				
-	REGISTER_ELEVATOR_EVENT_SLEEP						time.Duration 	= 	time.Millisecond * 10
-				
-	NUMBER_OF_FLOORS		 							int 			= 	4
-	ELEVATOR_DOOR_OPEN_DURATION 						time.Duration 	= 	time.Second * 3
-				
-	TIMEOUT_TIME_ORDER_TAKEN							time.Duration	= 	time.Millisecond * 100
-				
-	SHOULD_DISPLAY_ELEVATOR 							bool 			= 	false
-	SHOULD_DISPLAY_WORKERS								bool 			= 	false
-				
+	// Network
+
 	PORT_SERVER_DEFAULT 								int 			= 	9125
 	PORT_SERVER_WITH_TIMEOUT							int 			= 	9126
 
-	SHOULD_USE_PROCESS_PAIRS 							bool 			= 	false
+	// Process pairs
+
+	SHOULD_USE_PROCESS_PAIRS 							bool 			= 	true
 
 	BACKUP_PROCESS_ALIVE_MESSAGE_DEADLINE				time.Duration	=   time.Millisecond * 200
 	BACKUP_PROCESS_ALIVE_NOTIFICATION_SLEEP  			time.Duration	=   time.Millisecond * 15
 
 	BACKUP_FILE_NAME									string			= 	"backup.txt"
+
+	// Elevator controller
+
+	SHOULD_DISPLAY_WORKERS								bool 			= 	false
+
+	DISTRIBUTOR_ALIVE_NOTIFICATION_DELAY 				time.Duration 	=	time.Millisecond * 40
+	DISTRIBUTOR_CONNECTION_CHECK_DELAY 					time.Duration 	=	time.Millisecond * 80
+				
+	TIMEOUT_TIME_ORDER_TAKEN							time.Duration	= 	time.Millisecond * 100	
+
+	// Elevator
+
+	SHOULD_DISPLAY_ELEVATOR 							bool 			= 	false
+
+	NUMBER_OF_FLOORS		 							int 			= 	4
+
+	REGISTER_ELEVATOR_EVENT_SLEEP						time.Duration 	= 	time.Millisecond * 10
+		
+	ELEVATOR_DOOR_OPEN_DURATION 						time.Duration 	= 	time.Second * 3
 );
